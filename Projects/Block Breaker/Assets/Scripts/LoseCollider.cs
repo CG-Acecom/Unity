0f0ec636ab5df83d58710013b02f9399533b4bdf
Rogger794 <rogger.valverde@uni.pe>
2017-08-02 18:47:27 -0500

    Modificando LoseCollider script

--- Projects/Block Breaker/Assets/Scripts/LoseCollider.cs
+++ Projects/Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LoseCollider : MonoBehaviour {
+    public LevelManager levelmanager;
 
     void OnTriggerEnter2D(Collider2D trigger)
     {
         print("Trigger");
+        levelmanager.LoadLevel("Win");
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
