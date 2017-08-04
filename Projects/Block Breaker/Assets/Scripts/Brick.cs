c9fd178db10fa8519d272742c6c689dded7765c3
Daniel <kirovxq_@hotmail.com>
2017-08-04 00:55:07 -0500

    Mecánica de palanca
    Se creó nueva escena para el objeto prueba "palanca" donde se le aplicó
    un script con su controlador.
    P.D.: Previamente se explicó código C# para unity y IEnumerators
    genéricos.

--- Projects/Block Breaker/Assets/Scripts/Brick.cs
+++ Projects/Block Breaker/Assets/Scripts/Brick.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+public class Brick : MonoBehaviour
+{
 
-public class Brick : MonoBehaviour {
+    [SerializeField]
+    private GameObject _refBall;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private GameObject _refMessage;
+
+    //[SerializeField]
+    //private Text _refText;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Destroy(this.gameObject);
+        //this.gameObject.SetActive(true);
+        _refMessage.SetActive(true);
+        Destroy(_refBall);
+        StartCoroutine(WaitTime());
+    }
+
+    IEnumerator WaitTime()
+    {
+        yield return new WaitForSeconds(5);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Win");
+    }
+    //// Use this for initialization
+    //void Start () {
+
+    //}
+
+    //// Update is called once per frame
+    //void Update () {
+
+    //}
 }
---
+++ Projects/Block Breaker/Assets/Scripts/Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    [SerializeField]
+    private float multiplicador = 10f;
+    [SerializeField]
+    private float force = 10f;
+
+    void Update () {
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            //this.transform.position = new Vector3(this.transform.position.x + Time.deltaTime * multiplicador, this.transform.position.y, this.transform.position.z);
+            this.GetComponent<Rigidbody2D>().AddForce(new Vector2(force, 0f));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //this.transform.position = new Vector3(this.transform.position.x - Time.deltaTime * multiplicador, this.transform.position.y, this.transform.position.z);
+            this.GetComponent<Rigidbody2D>().AddForce(new Vector2(-force, 0f));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + Time.deltaTime * multiplicador, this.transform.position.z);
+            this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, force));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - Time.deltaTime * multiplicador, this.transform.position.z);
+            this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, -force));
+        }
+    }
+}
