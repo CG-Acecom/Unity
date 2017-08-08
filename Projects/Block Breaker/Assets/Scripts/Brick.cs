ed1bb400008bbbedcd1ce6e0bb296e0860e16718
Rogger794 <rogger_jrj794@hotmail.com>
2017-08-07 22:09:48 -0500

    Finalizado
    Se añadio un sistema de particulas para simular el humo, se agrego
    nuevos sonidos para el rebote del balon y el para los bricks

--- Projects/Block Breaker/Assets/Scripts/Brick.cs
+++ Projects/Block Breaker/Assets/Scripts/Brick.cs
@@ -11,21 +11,29 @@ public class Brick : MonoBehaviour
     private bool isBreakable;
 
     private LevelManager levelmanager;
-    private Ball _refBall;
+    //private Ball _refBall;
 
     [SerializeField]
     private Sprite[] hitSprites;
 
+    //[SerializeField]
+    //private GameObject _refMessage;
+
     [SerializeField]
-    private GameObject _refMessage;
+    private GameObject smoke;
+
+    [SerializeField]
+    private AudioClip crack;
 
     //[SerializeField]
     //private Text _refText;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(crack, transform.position);
         if (isBreakable)
             HandleHits();
+        Debug.Log(breackableCount);
         //this.gameObject.SetActive(true);
         //_refMessage.SetActive(true);
         //Destroy(_refBall);
@@ -39,6 +47,8 @@ public class Brick : MonoBehaviour
         {
             breackableCount--;
             levelmanager.BrickDestroyed();
+
+            PuffSmoke();
             Destroy(gameObject);
         }
         else
@@ -46,11 +56,25 @@ public class Brick : MonoBehaviour
             LoadSprites();
         }
     }
+
+    void PuffSmoke()
+    {
+        GameObject smokePuff = Instantiate(smoke, gameObject.transform.position, Quaternion.identity);
+        ParticleSystem ps = smokePuff.GetComponent<ParticleSystem>();
+        ParticleSystem.MainModule psmain = ps.main;
+        psmain.startColor = gameObject.GetComponent<SpriteRenderer>().color;
+    }
+
     void LoadSprites()
     {
         int spriteIndex = timesHit - 1;
-        if(hitSprites[spriteIndex])
+        if (hitSprites[spriteIndex] != null)
+        {
             this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }else
+        {
+            Debug.LogError("Missing Sprite");
+        }
     }
     /*IEnumerator WaitTime()
     {
@@ -67,7 +91,7 @@ public class Brick : MonoBehaviour
         {
             breackableCount++;
         }
-        _refBall = GameObject.FindObjectOfType<Ball>();
+        //_refBall = GameObject.FindObjectOfType<Ball>();
         levelmanager = GameObject.FindObjectOfType<LevelManager>();
     }
 
--- Projects/Block Breaker/Assets/Scripts/Paddle.cs
+++ Projects/Block Breaker/Assets/Scripts/Paddle.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public bool autoPlay = false;
+
+    private Ball _refBall;
+
+    // Use this for initialization
+    void Start () {
+        _refBall = GameObject.FindObjectOfType<Ball>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
+        if (!autoPlay)
+        {
+            MoveWithMouse();
+        }else
+        {
+            AutoPlay();
+        }
+	}
 
+    void AutoPlay() {
+        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
+        Vector3 ballPosition=_refBall.transform.position;
+
+        paddlePos.x = Mathf.Clamp(ballPosition.x, 0.5f, 15.5f);
+        this.transform.position = paddlePos;
+    }
+
+    void MoveWithMouse()
+    {
+        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
         float mousePosInBlocks = Input.mousePosition.x / Screen.width * 16.0f;
 
         paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
-
         this.transform.position = paddlePos;
-	}
+    }
 }
